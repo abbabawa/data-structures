7b3e13a334ddfe6740116b1687a11b7271f80343
Bawa Abba <abbabawa6@gmail.com>
2021-06-21 22:43:38 +0100

    Insert after, search, deleteNode and deleteNodeAt functions wrked on

--- LinkedList.js
+++ LinkedList.js
@@ -1,40 +1,116 @@
+//https://betterprogramming.pub/understanding-and-implementing-linked-lists-in-javascript-with-es6-c6f8720b38a
+class Node{
+    data
+    next
+    constructor(data, next = null){
+        this.data = data
+        this.next = next
+    }
+}
+
 class LinkedList{
-    #head = {value: null, next: null}
-    #values = []
-    addNode(nodeValue){
-        if (this.#head.value == null) {
-            this.#head = {value: nodeValue, next: null}
-        }else{
-            
-            if(this.#head.next == null){
-                this.#head.next = this.#values.length
-            }else{
-                this.#values[this.#values.length - 1].next = this.#values.length
-            }
-            this.#values[this.#values.length] = {value: nodeValue, next: null}
-            
+    head = null
+    size = 0
+    
+    insertAtHead(data){
+        if(!this.head){
+            this.head = new Node(data)
         }
-        //console.log(this.#values, this.#head)
+        else{
+            this.head = new Node(data, this.head)
+        }
+    }
+
+    insertAtTail(data){
+        if(!this.head){
+            this.head = new Node(data)
+            return 
+        }
+
+        let current = this.head
+        while(current.next){
+            current = current.next
+        }
+        current.next = new Node(data)
+    }
+
+    insertAfter(data, node){
+        let newNode = new Node(data)
+        newNode.next = node.next
+        node.next = newNode
+    }
+
+    search(data){
+        if (!this.head) {
+            return "List is empty"
+        }
+        let current = this.head
+        while(current){
+            if (current.data == data) {
+                return current
+            }else{
+                current = current.next
+            }
+        }
+        return "item not found"
+    }
+
+    deleteNode(data){
+        if (this.head == null) {
+            return "List is empty"
+        }
+        else if(this.head.data == data){
+            this.head = this.head.next
+            return
+        }
+        let current = this.head
+        let previous = this.head
+        while(current != null && current.data != data){
+            previous = current
+            current = current.next
+        }
+        if(current == null)
+            return
+        previous.next = current.next
+    }
+
+    //Function to delete the node at a particular position
+    deleteNodeAt(position){
+        let current = this.head
+        let previous = null
+        if(position == 0){
+            this.head = this.head.next
+            return
+        }
+
+        for(let i = 0; i< position && current.next != null; i++){//console.log(current)
+            previous = current
+            current = current.next
+        }console.log(current)
+        if (current == null || (current.next == null && current.data == null)) {
+            return
+        }
+        previous.next = current.next
+        current = null
     }
 
     printList(){
-        if (this.#head.value == null) {
-            console.log("Empty List")
-            return
-        }
-        console.log(this.#head.value)
-        let next = this.#head.next
-        while(next >= 0 && next != null){
-            console.log(this.#values[next].value)
-            next = this.#values[next].next
-        }
+        console.log(this.head)
     }
 }
 
 let list = new LinkedList()
-list.addNode(10)
-list.addNode(20)
-list.addNode(30)
-list.addNode(40)
+// list.insertAtHead(10)
+// list.insertAtHead(20)
+// list.insertAtHead(30)
+// list.insertAtTail(40)
+// let current = list.search(20)
+// list.insertAfter(394, current)
+// list.deleteNode(20)
 
+list.insertAtTail(2)
+list.insertAtTail(3)
+list.insertAtTail(1)
+list.insertAtTail(7)
+list.deleteNodeAt(60)
 list.printList()
